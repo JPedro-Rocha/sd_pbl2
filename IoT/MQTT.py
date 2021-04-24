b2d4f0d213a16621f7ce9fb6b3b4a3cc5f1fe308
daniel fernandes campos <dfc152@hotmail.com>
2021-04-23 22:01:06 -0300

    mudando path dos certificados pra fica os da maquina virtual

--- IoT/MQTT.py
+++ IoT/MQTT.py
@@ -5,11 +5,12 @@ from datetime import date
 
 
 client = AWSIoTPyMQTT.AWSIoTMQTTClient("meu_client")
-
+#C:\\Users\\dfc15\\Downloads\\
+#/home/ubuntu/sd_pbl2/IoT/
 client.configureEndpoint("at9zi9dd4t3sg-ats.iot.us-east-1.amazonaws.com", 8883)
-client.configureCredentials("C:\\Users\\dfc15\\Downloads\\AmazonRootCA1.pem", 
-                            "C:\\Users\\dfc15\\Downloads\\a504e91ba6-private.pem.key",
-                            "C:\\Users\\dfc15\\Downloads\\a504e91ba6-certificate.pem.crt")
+client.configureCredentials("/home/ubuntu/sd_pbl2/IoT/certificates/AmazonRootCA1.pem", 
+                            "/home/ubuntu/sd_pbl2/IoT/a504e91ba6-private.pem.key",
+                            "/home/ubuntu/sd_pbl2/IoT/a504e91ba6-certificate.pem.crt")
 
 client.connect()
 #print("Client Connected")
