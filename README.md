b56bfa947faf00e8b2105f2199829ab6a59e0ed4
daniel fernandes campos <dfc152@hotmail.com>
2021-05-01 14:27:47 -0300

    mechendo na estrutura de pastas do arquivo arduino

--- README.md
+++ README.md
@@ -1,1 +1,276 @@
-# sd_pbl2
+# Relatorio de sistema
+
+#entrando no IoT core
+
+primeiramente precisamos registra a nossa coisa no AWS e pega os certificados para podermos conectala no MQTT do aws. Para isso vamos logar na nossa conta. Entao procuramos por "IoT core"como na imagem a baixo:
+
+![Alt Text](img/entrando no IoT.png)
+
+#criando politica
+
+vamos em proteger, em politicas e em criar:
+
+
+![Alt Text](img/criar politica.png)
+
+colocamos um nome em nossa politica ( para o exemplo demos o nome de politicaNodeMcu ) e colocamos * em ação, marcamos permitir na checkbox e clicamos em criar:
+
+![Alt Text](img/nome_politica.png)
+
+# Registando a coisa no aws
+
+Entao no menu lateral vamos em gerenciar, depois em coisas e por fim criar:
+
+![Alt Text](img/criando coisa.png)
+
+Para registrar a coisa clicamos em criar uma única coisa:
+
+![Alt Text](img/criar uma unica coisa.png)
+
+Entao escolhemos um nome para a coisa ( no exemplo foi escolhido node_mcu) e cliclamos em proximo:
+
+![Alt Text](img/nome coisa.png)
+
+clicamos entao em criar certificados:
+
+![Alt Text](img/criar certificados.png)
+
+Entao fazemos o download de todos os certificados e o do nosso CA da amazon ( esses arquivos sao importantes e nao devem ser compartilhadas ou perdidos ), ao clicarmo me fazer download do CA da amazon vamor ser levados a outra pagina:
+
+![Alt Text](img/download certificados.png)
+
+nessa nova pagina vamos salvar o conteudo do amazon root CA 1 como arquivo de texto junto dos certificados. para tal clicamos com o direito em salvar conteudo do link como:
+
+![Alt Text](img/salvar CA.png)
+
+
+entao cliclamos em ativar e axenar uma politica:
+
+![Alt Text](img/ativar.png)
+
+Selecionamos a politica que criamos e em registra a cosia:
+
+![Alt Text](img/registra coisa.png)
+
+#upando o codigo na placa
+
+entao faremos o download do codigo do git na nossa maquina:
+
+![Alt Text](img/salvar_codigo_git.png)
+
+colocamos 1 copia de todos os certificados e do AmazonRootCA na pasta:  "sd_pbl2/arduino/certificados" e na pasta "sd_pbl2/IoT/certificates".
+entao abrimos o pronpt de comando na pasta "sd_pbl2/arduino/certificados" e digitamos:
+
+```
+openssl x509 -in {nome_do_arquivo_de_certificado} -out crt.der -outform DER
+openssl rsa -in {nome_do_do_arquivo_da_private_key} -out private.der -outform DER
+openssl x509 -in AmazonRootCA1.pem -out ca.der -outform DER
+```
+
+assim geramos os arquivos dos certificados que serao upados na placa.
+
+Caso esse passo nao de cereto pode ser nescessario baixa e instala o OpenSSL.
+
+Ao terminarmos esse passo na nossa pasta "sd_pbl2/arduino/certificados" devemos ter 7 arquivos: os 4 que baixamos do aws + ca.der + crt.der + private.der
+Entao movemos estes 3 arquivos que foram criados para a uma pasta "sd_pbl2/arduino/data". Ao terminarmos esses passos nossas pastas dentro de "sd_pbl2"/arduino" devem esta dessa forma:
+
+```
+-certificados
+--{Certificado_baixado_do_aws}
+--{chave_privada_baixado_do_aws}
+--{chave_publica_baixado_do_aws}
+--AmazonRootCA1.pem
+-data
+--ca.der
+--crt.der
+--private.der
+-arduino.ino
+```
+
+entao voltamos para a pasta "sd_pbl2/arduino" e abrimos o codigo. ( caso nao tenha instalado instale a ultima versao da IDE do arduino ou atualise caso desatualizada ). E iremos em ferramentas, placa e por fim gerenciador de placas...:
+
+![Alt Text](img/arduino_iniciox.png)
+
+Sera aberto uma janela e na barra de busca digitaremos: "EPS8266", e clicaremos em instala no elemento "esp8266":
+
+![Alt Text](img/instala_esp.png)
+
+Apos ter terminado de instala ao clicarmos em ferramentas, placa, ESP8266 e por fim em NodeMCU 1.0:
+
+![Alt Text](img/node_mcu.png)
+
+Entao com a placa conectada ao computador garantimos que os dados serao enviado pro lugar correto, em ferramentas vamos em porta e selecionamos a pota na qual a placa esta ( no exemplo indicado pelas setas pretas ) e por fim clicamos em ESP8266 Sketch Data Upload ( indicado pela seta vermelha:
+
+![Alt Text](img/update_data.png)
+
+Ao realizarmos isso estaremos upando os aquivos da pasta data para a placa e entao alteramos no codigo do arduino as variaveis: id_rede, senha_rede e AWS_endpoint.
+
+![Alt Text](img/codigo arduino.png)
+
+caso nao saiba seu endpoint no aws basta ir em , na tela do IoT core e em configurações e la voce encontrara o endpoint.
+
+IMG
+
+Tambem sera nescessario baixa 3 bibliotecas do arduino, para isso na IDE do arduino vamos em sketch, incluir biblioteca e gerenciar bibliotecas...:
+
+![Alt Text](img/gerencia_blib.png)
+
+Entao sera aberno uma janela e nela vamos digita o nome da biblioteca e clicar em instala, repetindo esse processo para todas as bibliotecas que nao tivernos. Sendo nescessario baixar:
+
+-PubSubClient
+-NTPClient
+-ArduinoJson
+
+aqui temos um exemplo da instalação da biblioteca ArduinoJson:
+
+![Alt Text](img/instalando ArduinoJson.png)
+
+Agora podemos upar o sketch para a placa pelo botao que contem uma seta no topo do menu:
+
+![Alt Text](img/upando_scktch.png)
+
+#Criando maquina virtual
+
+Para uparmos o servidor vamos de volta ao AWS e dessa vez abrimos o EC2 ao invez do IoT, para abrirmos o EC2 o passo é similar ao do IoT core porem dessa vez buscamos por EC2 no lugar de IoT core.
+
+Entao vamos em instancias, instancias e executar instância:
+
+![Alt Text](img/criar_instancia Ec2.png)
+
+Vamos em AMIs da comunidade, Ubuntu e selecionamos a primeira ( Ubunu-focal-20.04) : 
+
+![Alt Text](img/upando_ubuntu.png)
+
+Selecionamos a instancia que queremos, nesse projeto o servido sera rodado na instancia t2.micro ) e clicamos para ir em configure os security group:
+
+![Alt Text](img/maquina.png)
+
+Deixamos marcado Criar um grupo de segurança novo, clicamos em add rule 2 vezes, e colocamos 1 das caixas adicionadas com HTTP e outra como HTTPS:
+
+![Alt Text](img/http.png)
+
+Entao clicamos em verificar e ativar no canto inferior direito.
+
+Na nova tela que somos levados clicamos em executar. E vai aparecer para escolhermos uma chave, caso ja tenha 1 basta colocar na dropbox "Escolher um par de chaves existentes", para criar um par basta ir em na dropbox e colocar "Criar um novo par de chaves" e colocar o nome para essa chave no campo de baixo e clicar em download ( no exemplo colocamos o nome chave ), apos fazer o download o botao executar instancia estara disponivel e basta clicar nele:
+
+![Alt Text](img/chave.png)
+
+Entao seremos redirecionamos para uma pagina mostrando que a instancia foi lançada e clicaremos em exibir instancai:
+
+![Alt Text](img/instancia.png)
+
+Entao iremos a pagina onde temos nossas isntancias e clicaremos no nome da instancia que acabamos de criar, caso haja varias instancias basta filtra pela "Data de lançamento" e seleciona a mais recente
+
+Entao ao clicarmos no nome da instancia que acabamos de criar seremos jogados em uma pagina da instancia, entao anotaremos o endereço de IPv4 publico da instancia e clicaremos em "Conectar":
+
+![Alt Text](img/conectar.png)
+
+Entao clicaremos em cliente SSH e copiaremos o exemplo:
+
+![Alt Text](img/ssh.png)
+
+#upando o servidor
+
+Entao para usuarios linux abriremos a pasta que tem a chave que aabamos de baixa e executaremos o comando que copiamos e precionar enter, para windowns recomenda-se baixar o git bash e executar o git bash na pasta que foi colocada a chave baixada e colar o comando copiado e dar enter:
+
+![Alt Text](img/comand_no_git_bash.png)
+
+Entao digitamos yes e precionamos enter para adiciona essa maquina a hosts conhecimos e por fim estaremos dentro da maquina.
+
+A principio digitaremos os seguntes comandos para atualiza o SO:
+
+```
+sudo apt update
+```
+
+e apos o termino
+
+```
+sudo apt upgrade -y
+```
+
+para realizar todas as atualizações. Entao faremos :
+
+```
+git clone {link desse github}
+```
+
+e 
+
+```
+sudo apt install python3-pip
+```
+
+abrimos a pasta "sd_pbl2" com:
+
+```
+cd sd_pbl2/
+```
+
+e executamos:
+
+```
+sudo pip3 install -r requirements.txt
+```
+
+assim todos os pactes python serao instalados. entao vamos para a o arquvio views.py na pasta IoT fazendo:
+
+```
+cd IoT/
+nano views.py
+```
+
+e editamos trocamos o valo de __base_url__ pelo valor do IPv4 publico da nossa maquina, que anotamos alguns passos anteriore:
+
+```
+...
+
+__base_url__='{seU_IPv4}'
+
+def listar(request):
+...
+
+```
+
+Neste exeplo o ip da maquina é 54.205.211.76, entao colocamos esse valor entre aspas la:
+
+![Alt Text](img/ajustando_ip.png)
+
+entao salvamos e saimos ( pode ser feito com ctrl+x escolhendo a opção 'y' e dando enter ). Tambem precisamos upar nossos certificados no servidor ( pode tambem ser editado nos arquivos locais e upa esses arquivos no git ao invez de baixa direto desse git se baixa do que voce upou ). apos eles terem sido upados para la é nescessario que abramos o arquivo MQTT.py ( que se encontra na mesma pasta que estamos, a pasta do views.py ) com o comando:
+
+```
+nano MQTT.py
+```
+
+e editemos o path dos certificados e o colocamos nosso endpoint do AWS:
+
+```
+
+...
+client.configureEndpoint("{seu_end_point_do_aws}", 8883)
+client.configureCredentials("{path do /AmazonRootCA1.pem}",
+                            "{path_da_private_key}",
+                            "{path_do_certificado}")
+
+client.connect()
+...
+```
+
+pois sem esses dados nao sera poscivel usar o MQTT broker do AWS, para achar o path dos arquivos no servidor basta digita o comando "pwd" estando na pasta dos paths que entao sera escrito no terminal o path dessa pasta, bastando so adiciona o "/{nome_do_arquivo}" para se ter o caminho do mesmo. E entao voltamos uma pasta com:
+
+```
+cd ..
+```
+
+e executamos o seguinte comando:
+
+```
+sudo python3 manage.py runserver 0.0.0.0:80
+```
+
+Assim abrimos o servidor na porta :80 da maquina aberta para qualquer um acessar
+entao basta colocar o IPv4 da maquina do seu broser e vc estara acessando o site, garanta que esta acessando a porta 80 digitando: " HTTP://{IPv4} ".
+
+![Alt Text](img/Site.png)
+
+assim abrimos o site, ele ja vem com alguns dados pre colocados como por exemplo a coisa Lampada1 adicionada, 2 temporizadores para ela e algums dias de historico para mostra que esta tudo funcionando ok. Para mais informaçoes sobre a pagina olha o manual de Usuario
---
+++ arduino/arduino.ino
@@ -0,0 +1,304 @@
+#include "FS.h"
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#include <ArduinoJson.h>
+
+struct temporizador {
+  String tempo;
+  bool estado;
+  bool is_on = false;
+  bool will_be_trigger;
+};
+/*
+  SETUP PLACA
+*/
+
+//setando as informaçoes da rede
+const char* id_rede = "{ID_do_wifi}";
+const char* senha_rede = "{senha_do_wifi}";
+
+WiFiUDP udp;
+NTPClient tempo_obj(udp, "b.ntp.br", -3 * 3600, 60000);
+//endpoint AWS
+const char* AWS_endpoint = "{Seu_endpoint_do_aws}";
+//setando a função que vai ser chamada quando uma mensagem chegar
+void callback(char* topic, byte* payload, unsigned int length);
+
+WiFiClientSecure espClient;
+PubSubClient MQTT(AWS_endpoint, 8883, callback, espClient);
+
+/*
+  SETUP VARIAVEIS GLOBAIS
+*/
+
+// Variaveis pra o codigo
+String timer = "00:00:00";
+bool timer_on = false;
+
+int estado_lampada=1;
+unsigned long ligado_at;
+
+const int DEFOUT_JSON_BIT_SIZE = 256;
+
+const int len_temporizadores = 10;
+temporizador temporizadores[len_temporizadores];
+
+//Nome dispostivo
+const String nome_dispositivo = "Lampada1";
+
+/*
+  FUNÇOES
+*/
+
+/*
+  Esta função altera o estado da lampada
+  @param novo_estado é uma bool que sera o novo estado da lampada, true para ligado e false para desligado
+    para o caso de ligado o estado é alterado estado da variavel que  guarda o valor do estado da lampada, guarda o tempo no qual a lampada foi ligada e liga a lampada ( envia HIGH pro pino 2 )
+    para o caso de desligadao ele calcula o tempo que a lampada fico ligado e manda um publish no topico Soma_historico_Wh com o valor em milisegundos que a lampada fico on no codigo "tempo", muda a variavel de estado da lampada, zera a variavel que guarda o instante q a lampada estava on e apaga a lampada ( envia LOW no pino 2 )
+*/
+void mudar_estado_lampada( int novo_estado ) {
+  Serial.print("entrando na função de muda estado mudando para: ");
+  if (novo_estado == 1 && estado_lampada == 0) {
+    Serial.println("Ligando");
+    digitalWrite(LED_BUILTIN, LOW); // Manda LOW aqui pq a logica do led é invertida
+    ligado_at = tempo_obj.getEpochTime();
+    estado_lampada = novo_estado;
+  } else if (novo_estado == 0 && estado_lampada == 1) {
+    Serial.println("Apagando");
+    digitalWrite(LED_BUILTIN, HIGH); // manda HIGH aqui pq a logica do led é invertida
+    unsigned long now = tempo_obj.getEpochTime();
+    ligado_at = now - ligado_at;
+    char msg[75];
+    snprintf (msg, 75, "{\"tempo\": %uld}", ligado_at);
+    MQTT.publish("Lampada1/Alterar_Historico", msg);
+    Serial.print("historico alterado: ");
+    Serial.println(msg);
+    ligado_at = 0;
+    estado_lampada = novo_estado;
+  } else {
+    Serial.print ("nenhuma ação foi tomada variaveis de estatus-> estado atual: ");
+    Serial.print (estado_lampada);
+    Serial.print (" estado novo: ");
+    Serial.println (novo_estado);
+  }
+}
+/*
+  Esta função publica o estado da lampada como inteiro, 0 pra falso ( apagada ) e nao 0 pra true ( acessa ), no codigo "Estado"
+*/
+void publish_lampada() {
+  Serial.println( "enviando estado da lampada" );
+  char msg[60];
+  snprintf (msg, 60, "{\"Estado\": %d}", estado_lampada);
+  MQTT.publish("Lampada1/Estado", msg);
+}
+
+/*
+  Esta função seta o valor do timer, e seta a flag que indica se o timer esta ligado ou desligado
+  @param tempo, tipo String, sendo o horario que que o evento sera trigado, ex: caso timer seja de 30 min e o horario atual 15:32:23 o valor de tempo dever ser 16:02:23
+  @param is_ligado bool indicando se o timer deve ser ligado ou desligado
+*/
+void set_timer(String tempo, bool is_ligado) {
+  Serial.print("Criando timer para inverter o estado da lampada em (s):");
+  Serial.println(tempo);
+  timer_on = is_ligado == 1 ? true : false;
+  timer = tempo;
+}
+
+/*
+  Esta função publica o timer no broker MQTT
+*/
+void get_timer() {
+  Serial.print("Publicando timer");
+  char msg[160];
+  char buff[30];
+  for(int a =0; a<timer.length();a++){
+    buff[a]=timer[a];
+    }
+  buff[timer.length()] = 0;
+  if(timer_on){
+    snprintf (msg, 160, "{\"timer\": \"%s\", \"timer_on\":1}", buff);
+  }else{
+    snprintf (msg, 160, "{\"timer\": \"%s\", \"timer_on\":0}", buff);
+    }
+  Serial.println(msg);
+  MQTT.publish("Lampada1/get_timer", msg);
+}
+
+/*
+  Esta função atualiza os dados de um determinado temporizador do vetor.
+  @param tempom tipo String, sendo o horario que o temporizador deve ser acionado, ex: 19:34:59
+  @param estado um bool indicando se a lampada vai acender ou apagar
+  @param pos int que indica a posição do temporizador no vetor de temporizadores
+  @param is_add bool dizendo se o temporizador foi adicionado (true) ou removido (false)
+  @param should_be_trigger bool indicando se o temporizador deve ser trigado ainda nesse ciclo de 24h de funcionamento da placa
+*/
+void set_temporizador(String tempo, bool estado, int pos, bool is_add, bool should_be_trigger) {
+  temporizadores[pos].tempo = tempo;
+  temporizadores[pos].estado = estado;
+  temporizadores[pos].is_on = is_add == 1 ? true : false;
+  temporizadores[pos].will_be_trigger = should_be_trigger;
+}
+
+/*
+  Função que recebe a mensagem do MQTT e chama os devidos procedimentos para serem executados
+  @param topic é um vetor de char que contem o titulo do topico a qual a mensagme pertence e sera usadao para chamar o procedimento que lidara com essa mensagem
+  @param payload é um vetor de bytes e consiste no binario contendo a mensagem enviada
+  @param length é um unsigned int que tem o tamanho do payload
+*/
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.println("] ");
+  DynamicJsonDocument doc(DEFOUT_JSON_BIT_SIZE);
+  deserializeJson(doc, payload);
+  serializeJson(doc, Serial);
+  Serial.println();
+  if ( strcmp(topic,"Lampada1/OnOff") == 0 ) {
+    mudar_estado_lampada(doc["novo_estado"]);
+  } else if ( strcmp(topic, "Lampada1/get_tempo") == 0 ) {
+    publish_lampada();
+    get_timer();  
+  } else if ( strcmp(topic, "Lampada1/set_timer") == 0 ) {
+    set_timer(doc["set_timer"], doc["is_ligado"]);
+  } else if ( strcmp(topic, "Lampada1/temporizador") == 0 ) {
+    set_temporizador(doc["tempo"], doc["estado"], doc["posição"], doc["is_add"], doc["should_be_trigger"]);
+  };
+  topic = "";
+}
+/**
+* Este procedimento é responsavel por realizar a conecção com o wifi
+**/
+void conectar_wifi() {
+  // We start by connecting to a WiFi network
+  espClient.setBufferSizes(512, 512);
+  Serial.println();
+  Serial.print("Tentando conectar com: ");
+  Serial.println(id_rede);
+
+  WiFi.begin(id_rede, senha_rede);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(200);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi conectado");
+  tempo_obj.begin();
+  while (!tempo_obj.update()) {
+    tempo_obj.forceUpdate();
+  }
+  espClient.setX509Time(tempo_obj.getEpochTime());
+
+}
+/**
+*função para Conectar com o MQTT
+**/
+void conectar_mqtt() {
+  while (!MQTT.connected()) {
+    Serial.print("Tentando conectar com o MQTT: ");
+    // parque que faz o request pra conectar
+    if (MQTT.connect("ESPthing")) {
+      Serial.println("Conectado");
+      //se inscrevendo nos topicos que serao constantemente ouvidos pela placa
+      MQTT.subscribe("Lampada1/OnOff");
+      MQTT.subscribe("Lampada1/get_tempo");
+      MQTT.subscribe("Lampada1/set_timer");
+      MQTT.subscribe("Lampada1/temporizador");
+    } else {
+      Serial.print("Nao conectado, rc=");
+      Serial.print(MQTT.state());
+      Serial.println(" Nova tentativa em 1 segundo");
+      delay(1000);
+    }
+  }
+}
+/**
+* Função padrao do arduino, é executada 1 vez quando o despositivo entra em execução
+**/
+void setup() {
+  Serial.begin(9600); // inicia "console"
+  Serial.setDebugOutput(true);
+  // Pino do led como saida.
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(0, INPUT_PULLUP);
+  //tenta conectar com wifi
+  conectar_wifi();
+  delay(1000);
+  if (!SPIFFS.begin()) {
+    Serial.println("Falha na inicialização");
+    return;
+  }
+  for ( int a = 0; a < len_temporizadores; a++ )
+    temporizadores[a].is_on = false;
+
+  // Carregando certificados do AWS
+  File cert = SPIFFS.open("/crt.der", "r");
+  cert ? Serial.println("Certificado aberto com sucesso") : Serial.println("Falha ao abrir o certificado");
+  espClient.loadCertificate(cert) ? Serial.println("Certificado caregado") : Serial.println("Falha ao caregar o certificado");
+
+  File private_key = SPIFFS.open("/private.der", "r");
+  private_key ? Serial.println("Chave privada aberta com sucesso") : Serial.println("Falha em abrir a chave privada");
+  espClient.loadPrivateKey(private_key) ? Serial.println("Chave privada carregada com sucesso") : Serial.println("Chave privada não carregada");
+
+  File ca = SPIFFS.open("/ca.der", "r");
+  ca ? Serial.println("AWS CA aberto com sucesso") : Serial.println("AWS CA não foi abertoaberto");
+  espClient.loadCACert(ca) ? Serial.println("AWS CA caregado") : Serial.println("AWS CA não carregado");
+
+  conectar_mqtt();
+//  Serial.println(tempo_obj.getFormattedTime());
+  mudar_estado_lampada(1);
+  ligado_at = tempo_obj.getEpochTime();
+  MQTT.publish("Lampada1/Iniciar","{}");
+}
+
+bool was_button_pushed = false, was_already_sent = false;
+
+/**
+* É uma função padrao do arduino e sera executada infinitamente apos o setup até o despositivo ser desligado
+**/
+void loop() {
+  // se nao estiver conectado a amazon conecte
+  if (!MQTT.connected()) {
+    conectar_mqtt();
+  }
+  MQTT.loop();
+  //coletamos qual o millis de agora
+  String now = tempo_obj.getFormattedTime();
+  if (timer_on != false && now >= timer) { // se passamos do horario do timer  e ele nao foi ativado ainda ativamos ele
+    Serial.println("timer trigado"); 
+    mudar_estado_lampada( (estado_lampada == 0) ? 1 : 0 );
+   timer_on = false;
+  }
+  if (now == "00:00:00" ) { // a cada 24h re-armamos os temporizadores
+    for ( int a = 0; a < len_temporizadores; a++ )
+      temporizadores[a].will_be_trigger = true;
+    // Como deve ter o historico diario a cada final de dia se a lampada estiver acessa enviamos o tempo que ela ficou acessa e zeramos a variavel que guarda esse tempo na memoria
+    if(estado_lampada==1){
+      char msg[75];
+      snprintf (msg, 75, "{\"tempo\": %ld}", tempo_obj.getEpochTime() - ligado_at );
+      ligado_at = tempo_obj.getEpochTime();
+      MQTT.publish("Lampada1/Alterar_Historico", msg);
+    }
+    Serial.println( "Houve virada de dia" );
+  }// para cada temporizador o tempo de agora for maior que o de acionalo E não tivermos acionado ainda E ele deve ser usado o trigamos
+  for (int a = 0; a < len_temporizadores; a++) {
+    if ( temporizadores[a].will_be_trigger && now >= temporizadores[a].tempo && temporizadores[a].is_on) {
+      mudar_estado_lampada( temporizadores[a].estado );
+      temporizadores[a].will_be_trigger = false;
+    }
+  }// pin 0 é o botao
+  if ( was_button_pushed && !was_already_sent) {
+    if(WiFi.status() == WL_CONNECTED){
+      char msg[75];
+      snprintf (msg, 75, "{\"novo_estado\": %i}", estado_lampada == 1 ? 0 : 1);
+      MQTT.publish("Lampada1/OnOff" , msg);
+      }else{
+        mudar_estado_lampada(estado_lampada == 1 ? 0 : 1);
+        }
+    was_already_sent = true;
+  }
+  was_already_sent = was_button_pushed;
+  was_button_pushed = digitalRead(0) == 0 ? true : false;
+}
